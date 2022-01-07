171105361e83525e804be88dc234067073a3181a
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2022-01-07 14:58:29 +0100

    Correct typo in the path

--- sql-actions.sh
+++ sql-actions.sh
@@ -9,7 +9,7 @@
  echo
  echo "Starting database operations with flyway"
 
- docker run --net=host --rm -v $PWD/databases/dmo:/flyway/sql flyway/flyway migrate -url="$SNOWFLAKE_URL" -user="$SNOWFLAKE_USER" -password="$SNOWFLAKE_PASSWORD"
+ docker run --net=host --rm -v $PWD/databases/demo:/flyway/sql flyway/flyway migrate -url="$SNOWFLAKE_URL" -user="$SNOWFLAKE_USER" -password="$SNOWFLAKE_PASSWORD"
 
  echo "Successfully migrated sql scripts "
  echo
